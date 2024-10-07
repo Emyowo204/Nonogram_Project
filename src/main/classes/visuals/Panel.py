b6d177a75dc4650b571561ef6f27aba0f69fa2b7
Emy Gaete <167256512+Emyowo204@users.noreply.github.com>
2024-10-07 18:29:12 -0300

    Actualización para el manejo de colores en un panel genérico

--- src/main/classes/visuals/Panel.py
+++ src/main/classes/visuals/Panel.py
@@ -5,8 +5,10 @@ from src.main.classes.visuals.Componente import Componente
 class Panel(Componente):
     def __init__(self, x, y, width, height):
         super().__init__(x, y, width, height)
+        self.red = 0
+        self.green = 0
+        self.blue = 0
         self.surface = pygame.Surface((width,height))
-        self.surface.fill((255,0,0)) #Rojo
         self.container = []
 
     def setPos(self,x,y):
@@ -16,8 +18,14 @@ class Panel(Componente):
     def add(self, component):
         self.container.append(component)
 
-    def draw(self, destSurface): #desSurface es una superficie a la cual se va a dibujar, ejemplo de uso dibujar a pantalla(surface)
+    def setColor(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+    def draw(self, dest_surface): #desSurface es una superficie a la cual se va a dibujar, ejemplo de uso dibujar a pantalla(surface)
+        self.surface.fill((self.red, self.green, self.blue))
         for component in self.container:
-            component.draw(destSurface)
-        super().draw(self.surface)
-        destSurface.blit(self.surface,(self.x,self.y))
+            component.draw(dest_surface)
+        #super().draw(self.surface)
+        dest_surface.blit(self.surface, (self.x, self.y))
