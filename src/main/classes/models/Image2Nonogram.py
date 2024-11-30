1ca1d4ad910549c47b889977fab837710955b091
YinYanCat <chyanez2023@udec.cl>
2024-11-29 22:50:14 -0300

    feat: Crear código para transformar imagen a nonograma con color

--- src/main/classes/models/Image2Nonogram.py
+++ src/main/classes/models/Image2Nonogram.py
@@ -1,5 +1,10 @@
 import cv2
 import os
+
+import numpy as np
+from markdown.extensions.toc import unique
+
+
 class Image2Nonogram:
     """
         Clase para convertir una imagen en una matriz binaria, útil para
@@ -44,3 +49,38 @@ class Image2Nonogram:
         file.write(f"{width} {height}\n")
         for r in img_binary_matrix:
             file.write(" ".join(map(str,r))+"\n")
+
+    @staticmethod
+    def convertImg2Color(img_path, width, height):
+
+        fulldirectory = os.path.join(os.path.dirname(__file__), img_path)
+        img = cv2.imread(fulldirectory, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (width, height))
+        pixels = img.reshape((-1,3))
+        pixels = np.float32(pixels)
+
+        k=4
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+        _, labels, centers = cv2.kmeans(pixels, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+        centers = np.uint8(centers)
+        unique_colors = np.array(centers.tolist())
+        segmented_image = centers[labels.flatten()].reshape(img.shape)
+        color_matrix = segmented_image.reshape(-1, 3)
+
+        dir = os.path.join(os.path.dirname(__file__), '../../puzzles/Colored')
+        file_path = os.path.join(dir, os.path.splitext(os.path.basename(img_path))[0] + ".txt")
+
+        file = open(file_path, 'w')
+        file.write(f"{width} {height}\n")
+
+        for r in unique_colors:
+            file.write(f"{r} ")
+        file.write("\n")
+        for r in segmented_image:
+            for pixel in r:
+                index = np.where((unique_colors == pixel).all(axis=1))[0][0]
+                file.write(f"{index} ")
+            file.write("\n")
+
+        pass
